cc3101765de280a7164da976206f93a4fe6640fa
Lenart Kos <koslenart@gmail.com>
2022-11-27 16:13:53 +0100

    Premakni dtos folder v skupina02 package

---
+++ storitve/src/main/java/si/fri/prpo/skupina02/dtos/KosaricaDTO.java
@@ -0,0 +1,36 @@
+package si.fri.prpo.skupina02.dtos;
+
+import si.fri.prpo.skupina02.entitete.Izdelek;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class KosaricaDTO implements Serializable {
+    private Integer id;
+    private Integer uporabnikId;
+    private List<Integer> izdelki;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUporabnikId() {
+        return uporabnikId;
+    }
+
+    public void setUporabnikId(Integer uporabnikId) {
+        this.uporabnikId = uporabnikId;
+    }
+
+    public List<Integer> getIzdelki() {
+        return izdelki;
+    }
+
+    public void setIzdelki(List<Integer> izdelki) {
+        this.izdelki = izdelki;
+    }
+}
