0ae7dddb0b566f956dde74fe8c874120545a778b
Veronika Matek <matek.veronika@gmail.com>
2022-12-06 20:07:01 +0100

    Counter

--- storitve/src/main/java/si/fri/prpo/skupina02/storitve/crud/BelezenjeKlicevZrno.java
+++ storitve/src/main/java/si/fri/prpo/skupina02/storitve/crud/BelezenjeKlicevZrno.java
@@ -1,4 +1,17 @@
 package si.fri.prpo.skupina02.storitve.crud;
 
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+@ApplicationScoped
 public class BelezenjeKlicevZrno {
+    Map<String, Integer> counter = new HashMap<>();
+    Logger logger = Logger.getLogger(BelezenjeKlicevZrno.class.getName());
+    public void count(String method) {
+        counter.merge(method, 1, Integer::sum);
+        logger.info(counter.toString());
+    }
 }
--- storitve/src/main/java/si/fri/prpo/skupina02/storitve/interceptorji/BelezenjeKlicevInterceptor.java
+++ storitve/src/main/java/si/fri/prpo/skupina02/storitve/interceptorji/BelezenjeKlicevInterceptor.java
@@ -1,16 +1,25 @@
 package si.fri.prpo.skupina02.storitve.interceptorji;
 
 import si.fri.prpo.skupina02.storitve.anotacije.BeleziKlice;
+import si.fri.prpo.skupina02.storitve.crud.BelezenjeKlicevZrno;
 
+import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
 
 @Interceptor
 @BeleziKlice
 public class BelezenjeKlicevInterceptor {
 
-    @AroundInvoke
-    public void BelezenjeKlicev() {
+    @Inject
+    BelezenjeKlicevZrno belezenjeKlicevZrno;
 
+    @AroundInvoke
+    public Object BelezenjeKlicev(InvocationContext context) throws Exception {
+        String method = context.getMethod().toString();
+        belezenjeKlicevZrno.count(method);
+
+        return context.proceed();
     }
 }
